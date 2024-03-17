59cbff8c240ddb8ebe527c02f7628c13587e897e
jjasonhehe <jjasonhehe@gmail.com>
2024-03-17 01:32:42 -0700

    Create initial plan

---
+++ routine.py
@@ -0,0 +1,12 @@
+"""TODO"""
+
+class Routine:
+    # Initialize
+    # Method for adding 1 element
+    # Method for importing entire routine (list of elements?)
+    # Method for checking composition requirements
+    # Method for adding connections between elements
+    #   Not quite sure how I'll set up connections yet
+    # Method for calculating difficulty (with COP as an input parameter)
+    #   This way, when the dropdown for the COP is updated, we can call this method to recalculate
+    pass
