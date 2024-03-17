5d7e646cf88da4bd160ba70ba1c06e8cef561465
jjasonhehe <jjasonhehe@gmail.com>
2024-03-17 01:28:34 -0700

    Create subclasses for each apparatus

--- element.py
+++ element.py
@@ -7,10 +7,35 @@ Elements are movements defined by the FIG as individual skills of some set diffi
 class Element:
     """Base class for elements across apparatus.
     
-    Args:
+    Attributes:
         number (str): The box number assigned to an element in the COP.
     """
-    def __init__(self, number):
+    def __init__(self, cop, number):
+        self.cop = cop
         self.number = number
         self.description = None
         self.difficulty = None
+
+
+class VTElement(Element):
+    """Vault element."""
+    def __init__(self, number):
+        super().__init__(number)
+
+
+class UBElement(Element):
+    """Uneven bars element."""
+    def __init__(self, number):
+        super().__init__(number)
+
+
+class BBElement(Element):
+    """Balance beam element."""
+    def __init__(self, number):
+        super().__init__(number)
+
+
+class FXElement(Element):
+    """Floor exercise element."""
+    def __init__(self, number):
+        super().__init__(number)
